39373696345eb30d4a75a069ed328e45065249ee
Alex Xavier <alex.brq@seguradoralider.corp>
2020-03-12 15:28:21 -0300

    Notificações
    Término da funcionalidade de Notificações

--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -1,53 +1,76 @@
-import React from 'react';
-
+import React, { useState, useEffect, useMemo } from 'react';
 import { MdNotifications } from 'react-icons/md';
+import { parseISO, formatDistance } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
+import api from '~/services/api';
 
 import { Container, Badge, NotificationList, Scroll, NotificationItem } from './styles';
 
 export default function Notification() {
+  const [visible, setVisible] = useState(false);
+  const [notifications, setnotifications] = useState([]);
+
+  const hasUnread = useMemo(
+    () => !!notifications.find(notification => notification.read === false),
+    [notifications]
+  )
+
+  function handleToggleVisible() {
+    setVisible(!visible);
+    console.log('teste');
+  }
+
+  useEffect(() => {
+    async function loadNotifications() {
+      const response = await api.get('notifications');
+
+      const data = response.data.map(notification => ({
+        ...notification,
+        timeDistance: formatDistance(
+          parseISO(notification.createdAt),
+          new Date(),
+          { addSuffix: true, locale: pt }
+        )
+      }))
+
+      setnotifications(data);
+    }
+
+    loadNotifications();
+  }, []);
+
+  async function handleMarkAsRead(id) {
+    await api.put(`notifications/${id}`);
+
+    setnotifications(
+      notifications.map(notification =>
+        notification._id === id ? { ...notification, read: true } : notification)
+    );
+  }
+
   return (
     <Container>
-      <Badge hasUnread>
+      <Badge hasUnread={hasUnread} onClick={handleToggleVisible}>
         <MdNotifications color="#7159c1" size={20} />
       </Badge>
 
-      <NotificationList>
+      <NotificationList visible={visible}>
         <Scroll>
-          <NotificationItem unread>
-            <p>Você possui um novo agendamento para amanhã</p>
-            <time>há 2 dias</time>
-            <button type="button">Marcar como lida</button>
+          {notifications.map(notification =>(
+            <NotificationItem key={notification._id} unread={!notification.read}>
+            <p>{notification.content}</p>
+            <time>{notification.timeDistance}</time>
+            {!notification.read && (
+              <button
+                type="button"
+                onClick={() => handleMarkAsRead(notification._id)}
+              >
+                Marcar como lida
+              </button>
+            )}
           </NotificationItem>
-          <NotificationItem>
-            <p>Você possui um novo agendamento para amanhã</p>
-            <time>há 2 dias</time>
-            <button type="button">Marcar como lida</button>
-          </NotificationItem>
-          <NotificationItem>
-            <p>Você possui um novo agendamento para amanhã</p>
-            <time>há 2 dias</time>
-            <button type="button">Marcar como lida</button>
-          </NotificationItem>
-          <NotificationItem>
-            <p>Você possui um novo agendamento para amanhã</p>
-            <time>há 2 dias</time>
-            <button type="button">Marcar como lida</button>
-          </NotificationItem>
-          <NotificationItem>
-            <p>Você possui um novo agendamento para amanhã</p>
-            <time>há 2 dias</time>
-            <button type="button">Marcar como lida</button>
-          </NotificationItem>
-          <NotificationItem>
-            <p>Você possui um novo agendamento para amanhã</p>
-            <time>há 2 dias</time>
-            <button type="button">Marcar como lida</button>
-          </NotificationItem>
-          <NotificationItem>
-            <p>Você possui um novo agendamento para amanhã</p>
-            <time>há 2 dias</time>
-            <button type="button">Marcar como lida</button>
-          </NotificationItem>
+          ))}
         </Scroll>
       </NotificationList>
 
--- src/components/Notifications/styles.js
+++ src/components/Notifications/styles.js
@@ -34,6 +34,7 @@ export const NotificationList = styled.div`
   background: rgb(0, 0, 0, 0.6);
   border-radius: 4px;
   padding: 15px 5px;
+  display: ${props => (props.visible ? 'block' : 'none')};
 
   &::before {
     content: '';
@@ -66,11 +67,14 @@ export const NotificationItem = styled.div`
   p {
     font-size: 13px;
     line-height: 18px;
+    margin-bottom: 5px;
   }
 
   time {
+    display: block;
     font-size: 12px;
     opacity: 0.6;
+    margin-bottom: 5px;
   }
 
   button {
@@ -78,9 +82,6 @@ export const NotificationItem = styled.div`
     border: 0;
     background: none;
     color: ${lighten(0.2, '#7159c1')};
-    padding: 0 5px;
-    margin: 0 5px;
-    border-left: 1px solid rgba(255, 255, 255, 0.1);
   }
 
   ${props => props.unread &&
@@ -92,6 +93,7 @@ export const NotificationItem = styled.div`
         height: 8px;
         background: #ff892e;
         border-radius: 50%;
+        margin-left: 10px;
       }
     `
   }
